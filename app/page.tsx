dd200338f08fa5870abd2c2195a1c71eb830a6c0
adammosco <7vcjrdhfwf@privaterelay.appleid.com>
2026-06-24 18:53:28 +0200

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40, fontFamily: "sans-serif" }}>
+      <h1>Fanfirst MVP</h1>
+      <p>Get early access to gigs</p>
+
+      <a href="/event/1">
+        <button>Go to Demo Event</button>
+      </a>
+    </main>
+  );
+}
